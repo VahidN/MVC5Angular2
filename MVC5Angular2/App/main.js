d4594a2ee6dc40b326a5fab2d422de195e19ed03
VahidN <vahid_nasiri@yahoo.com>
2017-02-27 10:13:14 +0330

    Updated to V2.4.0

--- MVC5Angular2/App/main.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../node_modules/@types/core-js/index.d.ts" />
-/// <reference path="../node_modules/@types/node/index.d.ts" />
-/// <reference path="../node_modules/@types/jssha/index.d.ts" />
-/// <reference path="../node_modules/@types/jquery/index.d.ts" />
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
